79ce5d4d87ee6d2bde02dd29acec43d1328f9550
wdani <68472201+WilleLX1@users.noreply.github.com>
2025-06-12 02:16:03 +0200

    Add ping test app

--- Test_App/Form1.Designer.cs
+++ Test_App/Form1.Designer.cs
@@ -26,12 +26,45 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
+        private System.Windows.Forms.ListBox listBoxLog;
+        private System.Windows.Forms.Label labelStats;
+        private System.Windows.Forms.Timer timerPing;
+
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.listBoxLog = new System.Windows.Forms.ListBox();
+            this.labelStats = new System.Windows.Forms.Label();
+            this.timerPing = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+
+            // listBoxLog
+            this.listBoxLog.FormattingEnabled = true;
+            this.listBoxLog.ItemHeight = 15;
+            this.listBoxLog.Location = new System.Drawing.Point(12, 12);
+            this.listBoxLog.Name = "listBoxLog";
+            this.listBoxLog.Size = new System.Drawing.Size(360, 364);
+
+            // labelStats
+            this.labelStats.AutoSize = true;
+            this.labelStats.Location = new System.Drawing.Point(12, 382);
+            this.labelStats.Name = "labelStats";
+            this.labelStats.Size = new System.Drawing.Size(33, 15);
+            this.labelStats.Text = "Stats";
+
+            // timerPing
+            this.timerPing.Interval = 1000;
+            this.timerPing.Tick += new System.EventHandler(this.timerPing_Tick);
+
+            // Form1
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(384, 417);
+            this.Controls.Add(this.labelStats);
+            this.Controls.Add(this.listBoxLog);
+            this.Name = "Form1";
+            this.Text = "Ping Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
--- Test_App/Form1.cs
+++ Test_App/Form1.cs
@@ -2,9 +2,39 @@ namespace Test_App
 {
     public partial class Form1 : Form
     {
+        private readonly System.Net.NetworkInformation.Ping _ping = new();
+        private int _sent;
+        private int _success;
+        private long _totalTime;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void timerPing_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                var reply = await _ping.SendPingAsync("google.com", 1000);
+                _sent++;
+                if (reply.Status == System.Net.NetworkInformation.IPStatus.Success)
+                {
+                    _success++;
+                    _totalTime += reply.RoundtripTime;
+                    listBoxLog.Items.Insert(0, $"Reply from {reply.Address}: {reply.RoundtripTime}ms");
+                }
+                else
+                {
+                    listBoxLog.Items.Insert(0, $"Ping failed: {reply.Status}");
+                }
+
+                labelStats.Text = $"Sent: {_sent}, Success: {_success}, Avg: {( _success > 0 ? _totalTime / _success : 0)}ms";
+            }
+            catch (Exception ex)
+            {
+                listBoxLog.Items.Insert(0, $"Error: {ex.Message}");
+            }
         }
     }
 }
